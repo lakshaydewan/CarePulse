0a7b0cfca60e59a60227d508ce58528266bbab20
lakshaydiwan <laxhay666@gmail.com>
2024-09-18 19:42:39 +0530

    disable es-lint

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    eslint: {
+      ignoreDuringBuilds: true, // Disables ESLint checks during the build process
+    },
+  };
+  
+  export default nextConfig;
