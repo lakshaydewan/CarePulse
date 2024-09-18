4009daefb27372222642ee7843cffabf46b3c832
lakshaydiwan <laxhay666@gmail.com>
2024-09-18 22:06:18 +0530

    testing

--- components/Forms/RegistrationForm.tsx
+++ components/Forms/RegistrationForm.tsx
@@ -95,7 +95,7 @@ const PatientForm = () => {
           formData.append(key, value.toString());
         }
       });
-      const res = await axios.post("https://care-pulse-ten-flax.vercel.app/registration",
+      const res = await axios.post("https://care-pulse-ten-flax.vercel.app/api/registration",
           formData,
         {
           headers: {
